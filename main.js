832ef4ae8792daa55e8357299918eee31d9dbe9d
SimoneChiodo <simochi.005@gmail.com>
2025-09-03 20:14:59 +0200

    Add step 1: fighters take weapons

--- main.js
+++ main.js
@@ -1,1 +1,116 @@
-alert("Hello World!");
+// Dichiaro le liste "fighters" (combattenti) e "weapons" (armi)
+const fighters = [
+  {
+      name: 'Freezer',
+      power: 8000
+  },
+  {
+      name: 'Vegeta',
+      power: 8500
+  },
+  {
+      name: 'Crilin',
+      power: 500
+  },
+  {
+      name: 'Mr Satan',
+      power: 50
+  },
+  {
+      name: 'Junior',
+      power: 6000
+  },
+  {
+      name: 'Goku',
+      power: 9001
+  },
+  {
+      name: 'Tensing',
+      power: 450
+  },
+  {
+      name: 'Videl',
+      power: 300
+  },
+  {
+      name: 'Bulma',
+      power: 20
+  },
+  {
+      name: 'C-18',
+      power: 7800
+  },
+  {
+      name: 'Gohan',
+      power: 8900
+  },
+  {
+      name: 'Trunks',
+      power: 1250
+  }
+];
+const weapons = [
+  { 
+      name: "Ventaglio della Musa", 
+      power: 15 
+  },
+  { 
+      name: "Scouter", 
+      power: 30 
+  },
+  { 
+      name: "Bastone Roshi", 
+      power: 60 
+  },
+  { 
+      name: "Fagioli Magici", 
+      power: 70 
+  },
+  { 
+      name: "Katana di Yajirobei", 
+      power: 85 
+  },
+  { 
+      name: "Spada del Dragone Azzurro", 
+      power: 115 
+  },
+  { 
+      name: "Armatura Saiyan", 
+      power: 145 
+  },
+  { 
+      name: "Cannone da braccio", 
+      power: 170 
+  },
+  { 
+      name: "Nuvola d'oro", 
+      power: 200 
+  },
+  { 
+      name: "Bastone Nyoi", 
+      power: 220
+  },
+  { 
+      name: "Spada Z", 
+      power: 235 
+  },
+  { 
+      name: "Orecchini Potara", 
+      power: 250 
+  }
+];
+
+// Mostro i combattanti disarmati
+console.log("COMBATTENTI DISARMATI: ");
+console.log(JSON.stringify(fighters));
+
+// Ogni combattente prende un'arma random (questa non sarà più disponibile per gli altri)
+for(fighter of fighters) {
+  let randomIndex = Math.floor(Math.random() * weapons.length); // Prendo l'indice di un'arma casuale
+  fighter.weapon = weapons.splice(randomIndex, 1); // Il combattente prende l'arma dall'elenco
+}
+// Mostro i combattanti armati
+console.log("COMBATTENTI ARMATI: ");
+console.log(JSON.stringify(fighters));
+// Controllo che le armi siano vuote
+console.log(weapons);
