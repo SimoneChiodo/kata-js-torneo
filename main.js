b32f028df2e98462d67e248381c076a5db2f9d4c
SimoneChiodo <simochi.005@gmail.com>
2025-09-10 20:36:26 +0200

    Add step, graphic

--- main.js
+++ main.js
@@ -100,7 +100,8 @@ const weapons = [
   }
 ];
 
-let tournamentStarted = false;
+const buttonStart = document.getElementById("buttonStartTorunament");
+let step = 0;
 
 // Mostro i combattenti a schermo
 const fightersRow = document.getElementById("fightersRow");
@@ -119,82 +120,119 @@ fighters.map(fighter => {
 
 // Funzione per far partire il torneo
 function startTournament(){
-  if(tournamentStarted)
-    return;
-  const buttonStart = document.getElementById("buttonStartTorunament");
-  buttonStart.classList.add("disabled");
-  tournamentStarted = true;
+  updateStep(++step); // Aggiorno il testo sul pulsante
 
   // FASE 1: SCELTA DELL'ARMA ---------------
   // Mostro i combattanti disarmati
-  console.log("SCELTA DELL'ARMA ---------------");
-  console.log("COMBATTENTI DISARMATI: ");
-  console.log(convertJSON(fighters));
-
-  // Ogni combattente prende un'arma random (questa non sarà più disponibile per gli altri)
-  fighters.map(fighter => {
-    let randomIndex = randomNumber(0, weapons.length-1); // Prendo l'indice di un'arma casuale (il massimo non è incluso)
-    fighter.weapon = weapons.splice(randomIndex, 1)[0]; // Il combattente prende l'arma dall'elenco (NOTA: [0] rimuove l'oggetto dall'array)
-    document.getElementById(`cardWeapon${fighter.name}`).innerHTML = `<b>weapon:</b>  ${fighter.weapon.name}`;
-    document.getElementById(`cardWeaponPower${fighter.name}`).innerHTML = `<b>weapon power:</b>  ${fighter.weapon.power}`;
-  });
-  // Mostro i combattanti armati
-  console.log("COMBATTENTI ARMATI: ");
-  console.log(convertJSON(fighters));
-  // Controllo che le armi siano vuote
-  console.log(weapons);
+  if(step == 1) {
+    console.log("SCELTA DELL'ARMA ---------------");
+    console.log("COMBATTENTI DISARMATI: ");
+    console.log(convertJSON(fighters));
+  
+    // Ogni combattente prende un'arma random (questa non sarà più disponibile per gli altri)
+    fighters.map(fighter => {
+      let randomIndex = randomNumber(0, weapons.length-1); // Prendo l'indice di un'arma casuale (il massimo non è incluso)
+      fighter.weapon = weapons.splice(randomIndex, 1)[0]; // Il combattente prende l'arma dall'elenco (NOTA: [0] rimuove l'oggetto dall'array)
+      // AGGIORNO LA GRAFICA
+      document.getElementById(`cardWeapon${fighter.name}`).innerHTML = `<b>weapon:</b>  ${fighter.weapon.name}`;
+      document.getElementById(`cardWeaponPower${fighter.name}`).innerHTML = `<b>weapon power:</b>  ${fighter.weapon.power}`;
+    });
+    // Mostro i combattanti armati
+    console.log("COMBATTENTI ARMATI: ");
+    console.log(convertJSON(fighters));
+    // Controllo che le armi siano vuote
+    console.log(weapons);
+  }
 
   // FASE 2: ALLENAMENTO ---------------
   // Ogni combattente può moltiplicare la sua potenza per un numero tra 1 e 100
-  console.log("ALLENAMENTO ---------------");
-  fighters.map(fighter => {
-    if(randomNumber(1, 2) == 1) { // 1/2 possibilità di potenziarsi
-      fighter.power *= randomNumber(1, 100); // la potenza si moltiplica per un valore tra 1 e 100
-      console.log("POTENTE: " + convertJSON(fighter));
-    } else {
-      console.log("NORMALE: " + convertJSON(fighter));
-    }
-  });
+  if(step == 2) {
+    console.log("ALLENAMENTO ---------------");
+    fighters.map(fighter => {
+      if(randomNumber(1, 2) == 1) { // 1/2 possibilità di potenziarsi
+        fighter.power *= randomNumber(1, 100); // la potenza si moltiplica per un valore tra 1 e 100
+        console.log("POTENTE: " + convertJSON(fighter));
+        // AGGIORNO LA GRAFICA
+        document.getElementById(`cardPower${fighter.name}`).innerHTML = `<b>power:</b>  ${fighter.power}`;
+      } else {
+        console.log("NORMALE: " + convertJSON(fighter));
+      }
+    });
+  }
 
   // FASE 3: QUALIFICAZIONE ---------------
   // Mantengo solo i combattenti che hanno una potenza sopra i 2000
-  console.log("QUALIFICAZIONE ---------------");
-  console.log("ECCO TUTTI I COMBATTENTI: " + convertJSON(fighters));
-  fighters = fighters.filter(fighter => fighter.power >= 2000);
-  console.log("ECCO I COMBATTENTI CHE HANNO PASSATO LE QUALIFICAZIONI: " + convertJSON(fighters));
+  if(step == 3) {
+    console.log("QUALIFICAZIONE ---------------");
+    console.log("ECCO TUTTI I COMBATTENTI: " + convertJSON(fighters));
+    fighters = fighters.filter(fighter => {
+      const keep = fighter.power >= 2000;
+      if(!keep)
+        document.getElementById(`card${fighter.name}`).classList.add("disabled");
+
+      return keep;
+    });
+    console.log("ECCO I COMBATTENTI CHE HANNO PASSATO LE QUALIFICAZIONI: " + convertJSON(fighters));
+    // AGGIORNO LA GRAFICA
+  }
 
   // FASE 4: COMBATTIMENTO ---------------
   // Ogni combattente combatte con il successivo in lista 
   // Ogni combattente deve combattere solo una volta, nel caso siano dispari si aggiunge un robot combattente
   // In caso di parita vince chi viene prima nella lista
-  console.log("COMBATTIMENTO ---------------");
-  if(fighters.length % 2 !== 0) // Se i combattenti sono dispari
-    fighters.push({ name: 'Robot', power: 4000, weapon: { name: "Mani nude", power: 0 }}); // Aggiungo un combattente robot
+  if(step == 4) {
+    console.log("COMBATTIMENTO ---------------");
+    if(fighters.length % 2 !== 0) // Se i combattenti sono dispari
+      fighters.push({ name: 'Robot', power: 4000, weapon: { name: "Mani nude", power: 0 }}); // Aggiungo un combattente robot
 
-  console.log("COMBATTENTI PRONTI (" + fighters.length + "): " + convertJSON(fighters));
-  for(let i = 0; i < fighters.length; i++) {
-    console.log("NUOV ROUND");
-    
-    const power1 = fighters[i].power + fighters[i].weapon.power;
-    console.log("COMBATTENTE 1: " + convertJSON(fighters[i].name) + " + " + power1);
-    i++;
-    const power2 = fighters[i].power + fighters[i].weapon.power;
-    console.log("COMBATTENTE 2: " + convertJSON(fighters[i].name) + " + " + power2);
+    console.log("COMBATTENTI PRONTI (" + fighters.length + "): " + convertJSON(fighters));
+    for(let i = 0; i < fighters.length; i++) {
+      console.log("NUOV ROUND");
+      
+      const power1 = fighters[i].power + fighters[i].weapon.power;
+      console.log("COMBATTENTE 1: " + convertJSON(fighters[i].name) + " + " + power1);
+      i++;
+      const power2 = fighters[i].power + fighters[i].weapon.power;
+      console.log("COMBATTENTE 2: " + convertJSON(fighters[i].name) + " + " + power2);
 
-    if(power1 < power2) // Vince il secondo combattente
-      fighters[i-1].looser = true; // Segnalo i perdenti, per poi rimuoverli dopo
-    else if(power1 >= power2) // Vince il primo combattente o pareggiano
-      fighters[i].looser = true; // Segnalo i perdenti, per poi rimuoverli dopo
+      if(power1 < power2) // Vince il secondo combattente
+        fighters[i-1].looser = true; // Segnalo i perdenti, per poi rimuoverli dopo
+      else if(power1 >= power2) // Vince il primo combattente o pareggiano
+        fighters[i].looser = true; // Segnalo i perdenti, per poi rimuoverli dopo
+    }
+    fighters = fighters.filter(fighter => fighter.looser != true); // Rimuovo i perdenti
+    console.log("VINCITORI (" + fighters.length + "): " + convertJSON(fighters));
   }
-  fighters = fighters.filter(fighter => fighter.looser != true); // Rimuovo i perdenti
-  console.log("VINCITORI (" + fighters.length + "): " + convertJSON(fighters));
 
   // FASE 4: PREMIAZIONE ---------------
   // Mostro il podio composto da i primi 3 combattenti con la potenza maggiore, in ordine decrescente
-  let winners = fighters.sort((a, b) => b.power - a.power).slice(0, 3); // Creo una nuova variabile per il podio (NOTA: ASC -> "a.power - b.power")
-  console.log("SUL PODIO CI SONO:");
-  winners.map((winner, index) => console.log(index+1 + ", " + convertJSON(winner)));
+  if(step == 5) {
+    let winners = fighters.sort((a, b) => b.power - a.power).slice(0, 3); // Creo una nuova variabile per il podio (NOTA: ASC -> "a.power - b.power")
+    console.log("SUL PODIO CI SONO:");
+    winners.map((winner, index) => console.log(index+1 + ", " + convertJSON(winner)));
+  }
 }
+
+function updateStep(step){
+  switch(step){
+    case(1):
+      buttonStart.innerText = "Potenzia";
+      break;
+    case(2):
+      buttonStart.innerText = "Qualifica";
+      break;
+    case(3):
+      buttonStart.innerText = "Combattimento";
+      break;
+    case(4):
+      buttonStart.innerText = "Premiazione";
+      break;
+
+    default:
+      buttonStart.classList.add("disabled");
+  }
+}
+
 
 // Metodi HELPER
 /**
